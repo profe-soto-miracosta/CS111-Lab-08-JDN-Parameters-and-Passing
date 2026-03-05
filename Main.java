a74d7ff915e14f678a051f19dbacfb92c76e55c0
potun <potun3000@gmail.com>
2026-03-04 19:16:46 -0800

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,36 @@
-// PARTNER NAME: 
-// PARTNER NAME: 
-// CS111 SECTION #: 
-// DATE:
+/**
+ * Your project description here.
+ * @author Your name here
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        //DECLARATION + INITIALIZATION SECTION
+
+        //INPUT SECTION
+        // Still no input just yet...
+
+
+        //CALCULATION SECTION
+
+
+        //OUTPUT SECTION
+//        System.out.printf("Julian Day Number for birthday, 8/26/1918, is %d", calculateJDN(8,26,1918));
+
+    }
+
+    /**
+     *  Returns the Julian Day Number for a given month, day, ad year
+     * @param month
+     * @param day
+     * @param year
+     * @return the Julian Day Number
+     */
+//    public static int calcJDN(int month, int day, int year)
+    {
+
+    }
+
+}
